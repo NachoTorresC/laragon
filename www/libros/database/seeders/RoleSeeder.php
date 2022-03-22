5723cd3015964fee07f3a321bcda71e1dc708f4f
Nacho <vyd15559@educastur.es>
2022-03-22 19:52:44 +0100

    actualizado roles

--- www/libros/database/seeders/RoleSeeder.php
+++ www/libros/database/seeders/RoleSeeder.php
@@ -17,10 +17,10 @@ class RoleSeeder extends Seeder
     public function run()
     {
         $role1=Role::create(['name'=>'admin']);
-        $role2=Role::create(['name'=>'invitado']);
-        $role3=Role::create(['name'=>'usuario']);
+        $role2=Role::create(['name'=>'usuario']);
+       
         Permission::create(['name'=>'admin'])->assignRole($role1);
         Permission::create(['name'=>'admin.list_user'])->assignRole($role1);;
-        Permission::create(['name'=>'admin.list_libros'])->syncRoles([$role1,$role2,$role3]);
+        Permission::create(['name'=>'admin.list_libros'])->syncRoles([$role1,$role2]);
     }
 }
