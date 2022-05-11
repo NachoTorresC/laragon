9283ced62ccfcd038b3f41cb6cdacd9860936de8
Nacho <vyd15559@educastur.es>
2022-05-11 14:30:55 +0200

    modificando formularios

--- www/educonet/resources/views/home.blade.php
+++ www/educonet/resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @section('content')
 <main class="sm:container sm:mx-auto sm:mt-10">
--- www/educonet/resources/views/welcome.blade.php
+++ www/educonet/resources/views/welcome.blade.php
@@ -12,8 +12,11 @@
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+ 
+    
+
 </head>
-<body class="bg-gray-100 h-screen antialiased leading-none font-sans">
+<body class="bg-blue-400 h-screen antialiased leading-none font-sans">
 <div class="flex flex-col">
     @if(Route::has('login'))
         <div class="absolute top-0 right-0 mt-4 mr-4 space-x-4 sm:mt-6 sm:mr-6 sm:space-x-6">
@@ -34,7 +37,7 @@
                 <h1 class="mb-6 text-gray-600 text-center font-light tracking-wider text-4xl sm:mb-8 sm:text-6xl">
                     {{ config('app.name', 'Laravel') }}
                 </h1>
-                <ul class="flex flex-col space-y-2 sm:flex-row sm:flex-wrap sm:space-x-8 sm:space-y-0">
+           {{--      <ul class="flex flex-col space-y-2 sm:flex-row sm:flex-wrap sm:space-x-8 sm:space-y-0">
                     <li>
                         <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Documentation">Documentation</a>
                     </li>
@@ -59,7 +62,7 @@
                     <li>
                         <a href="https://tailwindcss.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Tailwind Css">Tailwind CSS</a>
                     </li>
-                </ul>
+                </ul> --}}
             </div>
         </div>
     </div>
