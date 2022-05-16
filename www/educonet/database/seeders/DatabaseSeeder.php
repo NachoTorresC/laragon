f346ea8ab72b36358c5da1aaf7f8ddf258ad780b
Nacho <vyd15559@educastur.es>
2022-05-16 18:21:56 +0200

    completado spatie

--- www/educonet/database/seeders/DatabaseSeeder.php
+++ www/educonet/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RoleSeeder::class);
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'name' => 'Nacho',
+            'email' => 'vyd15559@educastur.es',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => 'Ka18aprTpW7QFVtyuufd9n8aUvdbldx9XR7V8jM4SI2uEEyxLjetB02uRzDV'
+            ])->assignRole('Admin');
+         \App\Models\User::factory(9)->create();
+        $this->seedRelationRolesUser();
+
+        
+    }
+    
+
+    public function seedRelationRolesUser()
+    {
+        $users = \App\Models\User::all();
+
+        //Asigno a todos los users menos al admin el rol de guest
+        foreach ($users as $user) {
+            if ($user!=\App\Models\User::find(1)) {
+                $user->assignRole('guest');
+            }
+        }
     }
 }
