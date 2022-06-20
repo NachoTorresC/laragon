41fc4e11c794af0c72a0cfe16a4c2811d5de75b7
Nacho <vyd15559@educastur.es>
2022-06-20 20:11:02 +0200

    pdf actualizado

--- www/educonet/database/migrations/2022_05_10_161822_recursos.php
+++ www/educonet/database/migrations/2022_05_10_161822_recursos.php
@@ -34,11 +34,11 @@ class Recursos extends Migration
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Marcasitios modelo arcoíris.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Cristina  ',
             'categoria'=>'Asamblea',
             'descripcion'=>'Conjunto de 12 modelos diferentes de arcoíris para marcar los lugares en los que sentarse en la zona de asamblea.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Marcasitiosmodeloarcoiris.pdf',
             'Imagen'=>'1.png'
         ]);
 
@@ -49,17 +49,17 @@ class Recursos extends Migration
             'categoria'=>'Asamblea',
             'descripcion'=>'Plantilla de horario semanal (de lunes a viernes) distribuida en 5 sesiones diarias y descanso intermedio. Incluye columna para las horas.',
             'id_profesores'=>'3',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Horario.pdf',
             'Imagen'=>'1.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Tarjetas de cumpleaños.',
-            'autor' =>'David ',
+            'autor' =>'Nico ',
             'categoria'=>'Asamblea',
             'descripcion'=>'Tarjetas de los meses del año en las que añadir las fotos del alumnado según su mes de nacimiento.',
             'id_profesores'=>'2',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Tarjetadecumpleaños.pdf',
             'Imagen'=>'2.png'
         ]);
         DB::table("recursos")
@@ -69,7 +69,7 @@ class Recursos extends Migration
             'categoria'=>'Asamblea',
             'descripcion'=>'18 carteles con distintas rutinas y momentos habituales del aula para estructurar la jornada. En letra mayúscula y con imagen asociada.',
             'id_profesores'=>'4',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Rutinas.pdf',
             'Imagen'=>'3.png'
         ]);
         DB::table("recursos")
@@ -79,7 +79,7 @@ class Recursos extends Migration
             'categoria'=>'Asamblea',
             'descripcion'=>'Etiquetas en mayúscula, minúscula e imagen real de materiales y juguetes habituales.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Etiquetasdecajoneras.pdf',
             'Imagen'=>'4.png'
         ]);
         DB::table("recursos")
@@ -89,94 +89,94 @@ class Recursos extends Migration
             'categoria'=>'Lógico-matemática',
             'descripcion'=>'Imprimible para trabajar la asociación por colores y grafía-cantidad desde el 1 hasta el 10, añadiendo tantos botones como nos indique su sombrero.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Muñecosdenieve.pdf',
             'Imagen'=>'5.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Dino-series.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Adela',
             'categoria'=>'Proyectos (dinosaurios)',
             'descripcion'=>'Imprimible con temática de dinosaurios para trabajar vocabulario y nociones de seriación. Incluye series de dos y tres elementos simples, y patrones combinados.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Seriesdinosaurios.pdf',
             'Imagen'=>'6.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'La ruleta del cuerpo humano',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Mateo ',
             'categoria'=>'Proyectos (cuerpo humano)',
             'descripcion'=>'Conjunto de 50 imágenes para trabajar vocabulario relativo al cuerpo humano (partes externas, articulaciones, huesos, músculos, órganos, material médico…). .',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Partesdelcuerpo.pdf',
             'Imagen'=>'7.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'El bingo del cuerpo humano.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Laura ',
             'categoria'=>'Proyectos (cuerpo humano)',
             'descripcion'=>'25 cartones de bingo y sus correspondientes imágenes a modo de bolas con vocabulario relativo al cuerpo humano (partes externas, articulaciones, huesos, músculos, órganos, material médico…).',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Bingo.pdf',
             'Imagen'=>'8.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Dino-receta.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Cristina ',
             'categoria'=>'Proyectos (dinosaurios)',
             'descripcion'=>'Cartel con los ingredientes necesarios y pasos a seguir para construir tu propio diplodocus saludable. ¡Para comérselo!',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Dinoreceta.pdf',
             'Imagen'=>'9.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'La huevera de las vocales.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Cristina ',
             'categoria'=>'Lenguajes',
             'descripcion'=>'Un recurso para iniciarse en la conciencia fonológica con los sonidos de las vocales a través de la clasificación de las imágenes presentes en los huevos en su correspondiente huevera.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'HUEVERADEVOCALES.pdf',
             'Imagen'=>'10.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Palabras desordenadas.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Laura ',
             'categoria'=>'Lenguajes',
             'descripcion'=>'Conjunto de 12 tarjetas con letras desordenadas para formar las palabras. Diferentes niveles de dificultad (directas, inversas y trabadas).',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Ordenapalabras.pdf',
             'Imagen'=>'11.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Laberintos de otoño.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Cristina  ',
             'categoria'=>'Lenguajes',
             'descripcion'=>'Distintos patrones grafomotores bajo la temática del otoño que pueden imprimirse en distintos tamaños o proyectarse a gran escala en PDI.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Laberinto.pdf',
             'Imagen'=>'12.png'
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> '¡A la compra!',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Andrea ',
             'categoria'=>'Asamblea',
             'descripcion'=>'Con esta lista de la compra podrás trabajar la atención y el conteo de frutas y verduras (hasta el 10). Incluye dos niveles de dificultad: acompañamiento de imágenes para prelectores y palabra escrita.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Alacompra.pdf',
             'Imagen'=>'13.png'
 
         ]);
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Marcasitios modelo arcoíris.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'Cristina ',
             'categoria'=>'Lógico-matemática',
             'descripcion'=>'Conjunto de 12 modelos diferentes de arcoíris para marcar los lugares en los que sentarse en la zona de asamblea.',
             'id_profesores'=>'1',
@@ -186,11 +186,11 @@ class Recursos extends Migration
         DB::table("recursos")
         ->insert([
             'nombre'=> 'Mi casa.',
-            'autor' =>'Cristina Rionda ',
+            'autor' =>'María ',
             'categoria'=>'Lenguajes',
             'descripcion'=>'Libro imprimible con una habitación diferente en cada página, e imágenes de objetos cotidianos para clasificar según la estancia en la que estén.',
             'id_profesores'=>'1',
-            'descargable'=>'descargable1.pdf',
+            'descargable'=>'Lacasa.pdf',
             'Imagen'=>'15.png'
         ]);
       
